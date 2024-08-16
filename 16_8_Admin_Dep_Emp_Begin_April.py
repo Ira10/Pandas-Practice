04606dd8cd6e87f20e355b97a087ba732b75631a
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-08-16 20:12:22 +0530

    Create 16_8_Admin_Dep_Emp_Begin_April.py

---
+++ 16_8_Admin_Dep_Emp_Begin_April.py
@@ -0,0 +1,16 @@
+Admin Department Employees Beginning in April or Later.
+
+
+# Import your libraries
+import pandas as pd
+
+# Start writing code
+worker = worker[worker['department']=='Admin']
+worker_april = worker[worker['joining_date']>= '2014-04-01 00:00:00']
+worker_april.shape[0]
+
+
+
+
+
+https://docs.google.com/spreadsheets/d/1abXSPieLpPtnhRlpPWF7ukATxTnxNZw6mQnpTqnwT4s/edit?gid=1486747892#gid=1486747892
