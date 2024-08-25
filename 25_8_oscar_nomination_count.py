53fa54e0fbea550446d969248f732b991faaead1
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-08-25 12:36:06 +0530

    Create 25_8_oscar_nomination_count.py

---
+++ 25_8_oscar_nomination_count.py
@@ -0,0 +1,15 @@
+Count the number of movies that Abigail Breslin was nominated for an oscar.									
+
+									
+# Import your libraries						
+import pandas as pd						
+									
+# Start writing code						
+oscar_nominees[oscar_nominees['nominee']=='Abigail Breslin']['movie'].value_counts() #nunique() #count()		
+
+
+
+Output 
+1 
+
+https://docs.google.com/spreadsheets/d/1abXSPieLpPtnhRlpPWF7ukATxTnxNZw6mQnpTqnwT4s/edit?gid=957542970#gid=957542970									
