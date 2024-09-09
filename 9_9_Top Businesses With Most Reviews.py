a1aef9cec7ff634387958216b384dce273c37b38
Indrani Sarkar <84556248+Ira10@users.noreply.github.com>
2024-09-09 20:14:10 +0530

    Update 9_9_Top Businesses With Most Reviews.py

--- 9_9_Top Businesses With Most Reviews.py
+++ 9_9_Top Businesses With Most Reviews.py
@@ -1,1 +1,21 @@
+Top Businesses With Most Reviews						9th September 																			
+																									
+Find the top 5 businesses with most reviews. Assume that each row has a unique business_id such that the total reviews for each business is listed on each row. 																									
+Output the business name along with the total number of reviews and order your results by the total reviews in descending order.																									
+																									
+# Import your libraries																									
+import pandas as pd																									
+																									
+# Start writing code																									
+yelp_business.groupby('name')['review_count'].sum().reset_index(name='review_count').sort_values(by='review_count', ascending =False)[:5]																									
+																									
 
+
+
+Output																									
+name	review_count																								
+Iron Chef	331																								
+Jacs Dining and Tap House	197																								
+Grimaldi's Pizzeria	187																								
+Signs Restaurant	120																								
+Kassab's	101																								
